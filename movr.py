0912ee0dc83ab2ad65d4f6c9e706db7f9bcf9736
TK <tk@tkinvt.com>
2020-02-17 19:30:56 -0500

    Changed logic to deal with weird deluge behavior

--- movr.py
+++ movr.py
@@ -1,35 +1,55 @@
 import boto3
 import os
+import requests
 import shutil
+from dotenv import load_dotenv
+
+load_dotenv()
 
 dynamodb = boto3.resource('dynamodb')
 db = dynamodb.Table('downloadr')
 
-PATH_TO_SORTING_BOX = '/path/to/ExtMedia/Sorting_Box'
-PATH_TO_MOVIES = '/path/to/ExtMedia/Movies'
+# constants
+DELUGE_API_URL = os.getenv('DELUGE_API_URL')
+BOT_PASSWORD = os.getenv('BOT_PASSWORD')
+PATH_TO_SORTING_BOX = os.getenv('PATH_TO_SORTING_BOX')
+PATH_TO_MOVIES = os.getenv('PATH_TO_MOVIES')
 # PATH_TO_TV = ''
 # PATH_TO_AUDIOBOOK = ''
 # PATH_TO_MUSIC = ''
 
+def db_items():
+    items = db.scan()['Items']
+    return items
+
+def update():
+    for item in db_items():
+        if 'name' not in item:
+            id = item['id']
+            url = DELUGE_API_URL + "/" + id
+            r = requests.get(url, auth=('bot', BOT_PASSWORD))
+            name = r.json()['name']
+            db.update_item(Key={'id': id}, AttributeUpdates={'name': {'Value': name}})
+
 def move():
     # list of downloads to process
     # drawn from db, added by downloadr
-    downloads = [download['name'] for download in db.scan()['Items']]
+    downloads = db_items()
 
     with os.scandir(PATH_TO_SORTING_BOX) as scan:
         for item in scan:
             for download in downloads:
-                if item.name in download:
+                if item.name in download['name']:
                     # do something
-                    table_key = {'name': download}
+                    table_key = {'id': download['id']}
                     type = db.get_item(Key=table_key)['Item']['type']
                     if type == 'movie':
-                        dl_source = PATH_TO_SORTING_BOX + '/{}'.format(item.name)
-                        print(dl_source)
+                        dl_source = PATH_TO_SORTING_BOX + '/' + item.name
                         shutil.move(dl_source, PATH_TO_MOVIES)
+                        url = DELUGE_API_URL + '/' + download['id']
+                        requests.delete(url, auth=('bot', BOT_PASSWORD))
                         db.delete_item(Key=table_key)
 
 if __name__ == '__main__':
+    update()
     move()
-
-    # print(db.scan()['Items'])
